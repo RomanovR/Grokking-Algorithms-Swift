abae6eb8eb0b1538ba3eb9b5cd65485a08466b47
xdrond <xdrond@gmail.com>
2020-04-23 01:26:16 +0700

    Optimized calls of functions in cycle. Added .DS_Store to .gitignore.

--- Chapter 2(Selection sort).playground/Contents.swift
+++ Chapter 2(Selection sort).playground/Contents.swift
@@ -1,19 +1,6 @@
 import Foundation
 
-let testArray = [5, 6, 10, 7, 1, 0, 9, 4, 2, 15]
-
-func calculateTime(block : (() -> Void)) {
-    let start = DispatchTime.now()
-    block()
-    let end = DispatchTime.now()
-    let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
-    let timeInterval = Double(nanoTime) / 1_000_000_000
-    print("Time: \(timeInterval) seconds")
-}
-
-calculateTime {
-//    exampleFunc()// function whose execution time to be calculated
-}
+let testArray = [5, 6, 10, 7, 1, 0, 9, 4, 2, 15, -20]
 
 func getIndexOfSmallest(array: [Int]) -> Int {
     var smallestItem = array[0]
@@ -28,18 +15,16 @@ func getIndexOfSmallest(array: [Int]) -> Int {
     return indexOfSmallestItem + 1
 }
 
-
-
-getIndexOfSmallest(array: testArray)
-
+//getIndexOfSmallest(array: testArray)
 
 func selectionSort(inputArray: [Int]) -> [Int] {
     var unsortedArray = inputArray
     var sortedArray = [Int]()
-    var indexOfSmallest: Int = 0
+    var indexOfSmallest: Int
+    var smallestItem: Int
     for _ in inputArray {
-        print(unsortedArray)
-        indexOfSmallest = unsortedArray.firstIndex(of: unsortedArray.min()!)!
+        smallestItem = unsortedArray.min()!
+        indexOfSmallest = unsortedArray.firstIndex(of: smallestItem)!
         sortedArray.append(unsortedArray.min()!)
         unsortedArray.remove(at: indexOfSmallest)
     }
