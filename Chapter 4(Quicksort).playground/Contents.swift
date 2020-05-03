6357d1b724dae59669cb4443af3fcc166e1e95c9
xdrond <xdrond@gmail.com>
2020-05-03 17:58:06 +0700

    Exercise  4.2(recursive count).

--- Chapter 4(Quicksort).playground/Contents.swift
+++ Chapter 4(Quicksort).playground/Contents.swift
@@ -1,7 +1,6 @@
-// Ex 4.1
 let exampleArray = [1, 2, 3, 10, 4, 5, 6]
-exampleArray.count
 
+// Ex 4.1
 func recSum(source: [Int], _ total: Int = 0) -> Int {
     if let head = source.first {
         let tail = Array(source.dropFirst())
@@ -14,7 +13,18 @@ func recSum(source: [Int], _ total: Int = 0) -> Int {
 
 recSum(source: exampleArray)
 
+// Ex 4.2
+func recCount(source: [Int], _ count: Int = 0) -> Int {
+    if let _ = source.first {
+        let tail = Array(source.dropFirst())
+        return recCount(source: tail, count + 1)
+    }
+    else{
+        return count
+    }
+}
 
+recCount(source: exampleArray)
 
 // Ex 4.3
 func recMax(source: [Int], _ max: Int = 0) -> Int {
